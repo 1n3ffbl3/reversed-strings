3f524935047bcc9e0b8d05dc6f32cca5ff6a8fbe
1n3ffbl3 <37382210+1n3ffbl3@users.noreply.github.com>
2018-04-06 15:53:28 +0200

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,3 +1,24 @@
 // Your code here
 
+function reversed_string(str){
+  let str_reversed = ""
+  
+  if (string is even) {
+    // reverse an even word
+    let length = lentgh of string;
+    for (let i =0;  length/2;  i++;){
+      // swap the ith and (length-1)-i 'th letters
+    };
+  } else{
+    // reverse an odd word
+    let length = lentgh of string -1;
+    for (let i =0;  length/2;  i++;){
+      // swap the ith and (length-1)-i 'th letters
+    };
+    
+  }
+  
+  return str_reversed;
+};
+
 // Be sure it's the cleanest code you can write!
